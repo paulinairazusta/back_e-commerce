a1e66d712868605435cad43b26cb8ae9f97a81a1
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-22 14:14:41 -0300

    controller populate

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,10 @@
 const Product = require("../models/productModel");
+const Category = require("../models/categoryModel");
 
 const productController = {
 	getAllProducts: async (req, res) => {
-		const products = await Product.find();
+		const products = await Product.find().populate("category");
+		console.log(products);
 
 		res.send(products);
 	},
