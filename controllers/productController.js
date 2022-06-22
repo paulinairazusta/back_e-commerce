22f2669c2c0bfbd06f71ad3fd41bea2f96b734d8
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-22 14:16:53 -0300

    Ruta para un producto solo

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,12 +2,17 @@ const Product = require("../models/productModel");
 const Category = require("../models/categoryModel");
 
 const productController = {
-	getAllProducts: async (req, res) => {
-		const products = await Product.find().populate("category");
-		console.log(products);
+  getAllProducts: async (req, res) => {
+    const products = await Product.find().populate("category");
+    console.log(products);
 
-		res.send(products);
-	},
+    res.send(products);
+  },
+  getProductById: async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    console.log(product);
+    res.send(product);
+  },
 };
 
 module.exports = productController;
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const productController = require("../controllers/productController");
 const userController = require("../controllers/userController");
 
 router.get("/api/products", productController.getAllProducts);
+router.get("/api/product/:id", productController.getProductById);
 
 router.get("/api/users", userController.getAllUsers);
 
