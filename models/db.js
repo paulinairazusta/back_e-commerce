d1de6fa81e524494eac256f32e0210a8e8114da2
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-23 15:37:04 -0300

    productos destacados

--- models/db.js
+++ models/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-	mongoose.connect(process.env.CONNECTION_DB);
+  mongoose.connect(process.env.CONNECTION_DB);
 
-	// mongoose.connection
-	//   .once("open", () => {
-	//     console.log("Conexión establecida");
-	//   })
-	//   .on("error", (error) => {
-	//     console.log(error);
-	//   });
+  // mongoose.connection
+  //   .once("open", () => {
+  //     console.log("Conexión establecida");
+  //   })
+  //   .on("error", (error) => {
+  //     console.log(error);
+  //   });
 
-	await require("../seeders/userSeeder")(); // poner "()" al final para volver a crear los datos.
-	await require("../seeders/categorySeeder")(); //();
-	await require("../seeders/productSeeder")(); //();
+  await require("../seeders/userSeeder"); // poner "()" al final para volver a crear los datos.
+  await require("../seeders/categorySeeder"); //();
+  await require("../seeders/productSeeder"); //();
 };
