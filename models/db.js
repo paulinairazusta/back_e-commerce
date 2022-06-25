67312857a50f8a3435bcc1a411eb374e1633ea49
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-25 12:32:01 -0300

    Modelo User
    -Datos obligatorios
    -Email único
    -Password hasheadas
    -Hook de compración de passwords

--- models/db.js
+++ models/db.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  mongoose.connect(process.env.CONNECTION_DB);
+	mongoose.connect(process.env.CONNECTION_DB);
 
-  // mongoose.connection
-  //   .once("open", () => {
-  //     console.log("Conexión establecida");
-  //   })
-  //   .on("error", (error) => {
-  //     console.log(error);
-  //   });
-
-  await require("../seeders/userSeeder"); // poner "()" al final para volver a crear los datos.
-  await require("../seeders/categorySeeder"); //();
-  await require("../seeders/productSeeder"); //();
+	await require("../seeders/userSeeder")(); // poner "()" al final para volver a crear los datos.
+	await require("../seeders/categorySeeder"); //();
+	await require("../seeders/productSeeder"); //();
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,39 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const User = Schema({
-  firstname: String,
-  lastname: String,
-  email: String,
-  password: String,
-  address: String,
-  tel: Number,
-  orderList: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Order",
-    },
-  ],
+	firstname: String,
+	lastname: String,
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	address: String,
+	tel: Number,
+	orderList: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Order",
+		},
+	],
 });
+
+//middleware que se ejecuta antes de guardar y hashea las passwords
+//no funciona con arrow function!!
+User.pre("save", async function (next) {
+	this.password = await bcrypt.hash(this.password, 10);
+
+	next();
+});
+//busca un usuario y lo compara con la password que le llega
+User.methods.comparePass = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", User);
