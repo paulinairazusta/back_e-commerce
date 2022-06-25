feb5ae0a85e56c72bfd0a71601d90757dbc0fae1
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-25 14:39:16 -0300

    ruta nuevo producto
    --hay que checkear si ya existe y no crearlo solo aumentar el stock!

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,28 @@
 const Product = require("../models/productModel");
-const Category = require("../models/categoryModel");
 
 const productController = {
-  getAllProducts: async (req, res) => {
-    const products = await Product.find().populate("category");
+	getAllProducts: async (req, res) => {
+		const products = await Product.find().populate("category");
 
-    res.send(products);
-  },
-  getProductById: async (req, res) => {
-    const product = await Product.findById(req.params.id).populate("category");
+		res.send(products);
+	},
+	getProductById: async (req, res) => {
+		const product = await Product.findById(req.params.id).populate("category");
 
-    res.send(product);
-  },
+		res.send(product);
+	},
+	createProduct: async (req, res) => {
+		await Product.create({
+			name: req.body.name,
+			description: req.body.description,
+			price: req.body.price,
+			image: req.body.image,
+			destacado: req.body.destacado,
+			slug: "",
+			category: req.body.category, //Hay que pasarle el id de la categoria!!
+		});
+		res.send("producto agregado!");
+	},
 };
 
 module.exports = productController;
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const categoryController = require("../controllers/categoryController");
 
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/product/:id", productController.getProductById);
+router.post("/api/product", productController.createProduct);
 
 router.get("/api/users", userController.getAllUsers);
 router.get("/api/user/:id", userController.getUserById);
