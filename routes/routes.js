d5405ea6d8983290eae35cd99f66bea71c50cf10
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-27 14:26:28 -0300

    rutas de usuario

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const productController = require("../controllers/productController");
 const userController = require("../controllers/userController");
 const categoryController = require("../controllers/categoryController");
+//importamos lo necesario para usar el middleware de jwt
+const jwt = require("jsonwebtoken");
+const { expressjwt: checkJwt } = require("express-jwt");
 
 router.get("/api/products", productController.getAllProducts);
 router.get("/api/product/:id", productController.getProductById);
@@ -17,4 +20,13 @@ router.post("/api/login", userController.login);
 router.get("/api/categories", categoryController.getAllCategories);
 router.get("/api/category/:id", categoryController.getCategoryById);
 
+//ruta de prueba para el middleware JWT
+// router.use(checkJwt({ secret: process.env.SECRET, algorithms: ["HS256"] }));
+router.get(
+	"/prueba",
+	checkJwt({ secret: process.env.SECRET, algorithms: ["HS256"] }),
+	(req, res) => {
+		res.send(req.user);
+	}
+);
 module.exports = router;
