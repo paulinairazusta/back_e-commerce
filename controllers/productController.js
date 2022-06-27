a8cb761b1209fff805ea79b2bfdd4daac47b6739
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-27 15:30:23 -0300

    api para filtrar por categorías

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,28 +1,52 @@
 const Product = require("../models/productModel");
 
 const productController = {
-	getAllProducts: async (req, res) => {
-		const products = await Product.find().populate("category");
+  getAllProducts: async (req, res) => {
+    const products = await Product.find().populate("category");
 
-		res.send(products);
-	},
-	getProductById: async (req, res) => {
-		const product = await Product.findById(req.params.id).populate("category");
+    res.send(products);
+  },
+  getProductById: async (req, res) => {
+    const product = await Product.findById(req.params.id).populate("category");
 
-		res.send(product);
-	},
-	createProduct: async (req, res) => {
-		await Product.create({
-			name: req.body.name,
-			description: req.body.description,
-			price: req.body.price,
-			image: req.body.image,
-			destacado: req.body.destacado,
-			slug: "",
-			category: req.body.category, //Hay que pasarle el id de la categoria!!
-		});
-		res.send("producto agregado!");
-	},
+    res.send(product);
+  },
+  getCakes: async (req, res) => {
+    const products = await Product.find({
+      category: "62b4bfb2188f710b435fbea5",
+    });
+    res.send(products);
+  },
+  getTruffles: async (req, res) => {
+    const products = await Product.find({
+      category: "62b4bfb2188f710b435fbea6",
+    });
+    res.send(products);
+  },
+  getCups: async (req, res) => {
+    const products = await Product.find({
+      category: "62b4bfb2188f710b435fbea7",
+    });
+    res.send(products);
+  },
+  getTarts: async (req, res) => {
+    const products = await Product.find({
+      category: "62b4bfb2188f710b435fbea8",
+    });
+    res.send(products);
+  },
+  createProduct: async (req, res) => {
+    await Product.create({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.body.image,
+      destacado: req.body.destacado,
+      slug: "",
+      category: req.body.category, //Hay que pasarle el id de la categoria!!
+    });
+    res.send("producto agregado!");
+  },
 };
 
 module.exports = productController;
--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-	mongoose.connect(process.env.CONNECTION_DB);
+  mongoose.connect(process.env.CONNECTION_DB);
 
-	await require("../seeders/userSeeder")(); // poner "()" al final para volver a crear los datos.
-	await require("../seeders/categorySeeder"); //();
-	await require("../seeders/productSeeder"); //();
+  await require("../seeders/userSeeder"); // poner "()" al final para volver a crear los datos.
+  await require("../seeders/categorySeeder"); //();
+  await require("../seeders/productSeeder"); //();
 };
