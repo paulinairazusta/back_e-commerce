10cbdc7b03a466fc4dede4514bd5ecca2acd38f0
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-07-04 12:39:48 -0300

    db config

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 routes(app);
 
 const db = require("./models/db");
-db();
+// db();
 
 app.listen(port, () => {
   console.log("Server running");
--- models/db.js
+++ models/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 module.exports = async () => {
   mongoose.connect(process.env.CONNECTION_DB);
 
-  await require("../seeders/userSeeder"); // poner "()" al final para volver a crear los datos.
-  await require("../seeders/categorySeeder"); //();
-  await require("../seeders/productSeeder"); //();
-  await require("../seeders/adminSeeder");
+  await require("../seeders/userSeeder")();
+  await require("../seeders/categorySeeder")();
+  await require("../seeders/productSeeder")();
+  await require("../seeders/adminSeeder")();
 };
