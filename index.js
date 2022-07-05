ef7bc6391051c7cd0212fa98818f7c544e09dea9
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-07-05 20:40:12 -0300

    Comento blindaje momentáneamente

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(checkJwt({ secret: process.env.API_SECRET, algorithms: ["HS256"] }));
+// app.use(checkJwt({ secret: process.env.API_SECRET, algorithms: ["HS256"] }));
 
 routes(app);
 
