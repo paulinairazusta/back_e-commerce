4aba04ee75c2e80b99a1edf52c07e7d258f1a94d
Santiago Fantoni <fantonisantiago@gmail.com>
2022-07-08 00:11:49 -0300

    reset database

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,15 @@
 const adminRoutes = require("./adminRoutes");
 const categoryRoutes = require("./categoryRoutes.js");
+const dataBaseRouter = require("./dataBaseRoute");
 const orderRoutes = require("./orderRoutes");
 const productRoutes = require("./productRoutes");
 const userRoutes = require("./userRoutes");
 
 module.exports = (app) => {
-  app.use(adminRoutes);
-  app.use(categoryRoutes);
-  app.use(orderRoutes);
-  app.use(productRoutes);
-  app.use(userRoutes);
+	app.use(adminRoutes);
+	app.use(categoryRoutes);
+	app.use(orderRoutes);
+	app.use(productRoutes);
+	app.use(userRoutes);
+	app.use(dataBaseRouter);
 };
