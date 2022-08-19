7c6b697bc5ab9068042f7dabe513f25111bdf10e
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-08-18 23:33:10 -0300

    Update dataBaseRoute.js

--- routes/dataBaseRoute.js
+++ routes/dataBaseRoute.js
@@ -6,7 +6,7 @@ dataBaseRouter.post("/reset", async (req, res) => {
   await require("../seeders/categorySeeder")();
   await require("../seeders/productSeeder")();
   await require("../seeders/adminSeeder")();
-  await require("../seeders/orderSeeder");
+  await require("../seeders/orderSeeder")();
   res.json({ message: "Database reset" });
 });
 
